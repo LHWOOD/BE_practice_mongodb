183a5fe1ce5a45e8522c44a540ee91fd875c7e15
LHWOOD <hutsonwood@gmail.com>
2021-08-19 13:15:54 -0600

    created server connection

---
+++ models/user.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    trim: true,
+    required: "Username is Required",
+  },
+  email: {
+    type: String,
+    unique: true,
+  },
+
+  password: {
+    type: String,
+    trim: true,
+    required: "Password is Required",
+    validate: [({ length }) => length >= 6, "Password should be longer."],
+  },
+});
+
+const User = mongoose.model("User", UserSchema);
+module.exports = User;
--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3000;
+const db = require("./config/connection");
 
-const db = require("./models");
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -15,13 +15,17 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/practice", {
+mongoose.connect("mongodb://localhost/practice", {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
   useFindAndModify: false,
+  useUnifiedTopology: true,
 });
 
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+// routes
+app.use(require("./controllers/user.js"));
+
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`running on port ${PORT}!`);
+  });
 });
